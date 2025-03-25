5616b87a09bc9abafefdfb6954fee6ea2a9b2f64
Salenhard <bratiski@mail.ru>
2025-03-25 13:32:22 +1000

    second commit

--- models.py
+++ models.py
@@ -1,3 +1,5 @@
+from symtable import Class
+
 from app import app
 from config import db
 
@@ -11,47 +13,29 @@ class Country(db.Model):
     def __init__(self, name):
         self.name = name
 
+
 class City(db.Model):
     __tablename__ = 'city'
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column('Город', db.String(100), nullable=False)
     country_id = db.Column(db.Integer, db.ForeignKey('country.id'))
-    country = db.relationship("Country", back_populates = "cities")
+    country = db.relationship("Country", back_populates="cities")
 
     def __init__(self, name, country_id):
         self.name = name
         self.county_id = country_id
 
-class Maker(db.Model):
-    __tablename__ = 'maker'
+
+class Building(db.Model):
+    __tablename__ = "building"
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column('Производитель', db.String(100), nullable=False)
-    models = db.relationship("Model")
-    def __init__(self, name):
-        self.name = name
 
-class Model(db.Model):
-    __tablename__ = 'model'
+
+class TypeBuilding(db.Model):
+    __tablename__ = "typeBuilding"
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column('Модель машины', db.String(100), nullable=False)
-    maker_id = db.Column(db.Integer, db.ForeignKey('maker.id'))
-    maker = db.relationship("Maker", back_populates="models")
-    vehicles = db.relationship("Vehicle")
+    name = db.Column('Тип здания', db.String(100), nullable=False)
 
-    def __init__(self, name, maker_id):
-        self.name = name
-        self.maker_id = maker_id
-
-class Vehicle(db.Model):
-    __tablename__ = 'vehicle'
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column('Машина', db.String(100), nullable=False)
-    model_id = db.Column(db.Integer, db.ForeignKey('model.id'))
-    model = db.relationship("Model", back_populates="models")
-
-    def __init__(self, name, model_id):
-        self.name = name
-        self.maker_id = model_id
 
 app.app_context().push()
 
